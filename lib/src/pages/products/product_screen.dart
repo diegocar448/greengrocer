133fb3e7e7085001fc351f8f887d80600123dc2c
diegocar448 <diegocar448@hotmail.com>
2023-05-02 10:56:02 -0300

    Ajustes em product_screen e item_tile

--- lib/src/pages/products/product_screen.dart
+++ lib/src/pages/products/product_screen.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/material.dart';
+import 'package:greengrocer/src/models/item_model.dart';
 
 class ProductScreen extends StatelessWidget {
-  const ProductScreen({super.key});
+  //const ProductScreen({super.key});
+  const ProductScreen({
+    Key? key,
+    required this.item,
+  }) : super(key: key);
+
+  final ItemModel item;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white.withAlpha(230),
+      body: Column(
+        children: [
+          Expanded(
+            child: Image.asset(item.imgUrl),
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: const BorderRadius.vertical(
+                  top: Radius.circular(50),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.shade600,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
