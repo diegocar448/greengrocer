93529dbc322af8837978339582d2f7b9cac231ba
Diego Cardoso <diegocar448@hotmail.com>
2023-05-23 13:45:02 -0300

    SplashScreenFix finalizado

--- lib/src/pages/splash/splash_screen.dart
+++ lib/src/pages/splash/splash_screen.dart
@@ -1,22 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:greengrocer/src/config/custom_colors.dart';
+import 'package:greengrocer/src/pages/auth/sign_in_screen.dart';
 import 'package:greengrocer/src/pages/common_widgets/app_name_widget.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (c) {
+        return const SignInScreen();
+      }));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Material(
       child: Container(
         alignment: Alignment.center,
-        decoration: const BoxDecoration(
+        decoration: BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
             colors: [
-              Colors.blue,
-              Colors.purple,
-              Colors.red,
+              CustomColors.customSwatchColor,
+              CustomColors.customSwatchColor.shade700,
             ],
           ),
         ),
