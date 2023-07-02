057fce04eeb24f47d2f3bb4e3867f3dec772c48a
diegocar448 <diegocar448@hotmail.com>
2023-07-02 14:13:51 -0300

    Alterar quantidade de itens do carrinho (back/front)

--- lib/src/pages/cart/view/components/cart_tile.dart
+++ lib/src/pages/cart/view/components/cart_tile.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 import 'package:greengrocer/src/models/cart_item_model.dart';
+import 'package:greengrocer/src/pages/cart/controller/cart_controller.dart';
 import 'package:greengrocer/src/pages/common_widgets/quantity_widgets.dart';
 import 'package:greengrocer/src/services/utils_services.dart';
 
@@ -18,6 +20,7 @@ class CartTile extends StatefulWidget {
 
 class _CartTileState extends State<CartTile> {
   final UtilsServices utilsServices = UtilsServices();
+  final controller = Get.find<CartController>();
 
   @override
   Widget build(BuildContext context) {
@@ -59,6 +62,10 @@ class _CartTileState extends State<CartTile> {
           value: widget.cartItem.quantity,
           result: (quantity) {
             /* alterar o estado de quantity do cartItem */
+            controller.changeItemQuantity(
+              item: widget.cartItem,
+              quantity: quantity,
+            );
           },
           isRemovable: true,
         ),
