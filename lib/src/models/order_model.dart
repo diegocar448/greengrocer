01b4d88ec616788138d083504ac9035735beffc7
diegocar448 <diegocar448@hotmail.com>
2023-07-02 15:21:35 -0300

    Criando model para finalizacao de pedido

--- lib/src/models/order_model.dart
+++ lib/src/models/order_model.dart
@@ -1,21 +1,41 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:freezed_annotation/freezed_annotation.dart';
+
 import 'package:greengrocer/src/models/cart_item_model.dart';
 
+part 'order_model.g.dart';
+
+@JsonSerializable()
 class OrderModel {
   String id;
-  DateTime createdDateTime;
+  DateTime? createdDateTime;
+
+  @JsonKey(name: 'due')
   DateTime overdueDateTime;
+
+  // se não retornar nada do backend setamos como uma lista vazia
+  @JsonKey(defaultValue: [])
   List<CartItemModel> items;
   String status;
+
+  String qrCodeImage;
+  @JsonKey(name: 'copiaecola')
   String copyAndPaste;
   double total;
 
   OrderModel({
-    required this.copyAndPaste,
-    required this.createdDateTime,
     required this.id,
-    required this.items,
+    this.createdDateTime,
     required this.overdueDateTime,
+    required this.items,
     required this.status,
+    required this.qrCodeImage,
+    required this.copyAndPaste,
     required this.total,
   });
+
+  factory OrderModel.fromJson(Map<String, dynamic> json) =>
+      _$OrderModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$OrderModelToJson(this);
 }
